3d7e2c108bb979cc7e05d6f6bf331ef64df66e7c
lowercasebtw <jejojora2@gmail.com>
2026-06-09 20:12:21 -0500

    Remove Broken Disallowed Code

--- src/main/java/org/polyfrost/animatium_legacy/mixin/MinecraftMixin.java
+++ src/main/java/org/polyfrost/animatium_legacy/mixin/MinecraftMixin.java
@@ -2,7 +2,6 @@ package org.polyfrost.animatium_legacy.mixin;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
-import net.minecraft.client.multiplayer.PlayerControllerMP;
 import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.client.particle.EffectRenderer;
 import net.minecraft.client.renderer.EntityRenderer;
@@ -10,19 +9,15 @@ import net.minecraft.client.settings.GameSettings;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.EnumAction;
-import net.minecraft.item.ItemBlock;
 import net.minecraft.potion.Potion;
 import net.minecraft.util.BlockPos;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.MovingObjectPosition;
 import org.polyfrost.animatium_legacy.config.AnimatiumSettings;
 import org.polyfrost.animatium_legacy.hooks.SwingHook;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
@@ -71,7 +66,7 @@ public abstract class MinecraftMixin {
 
     @Inject(method = "clickMouse", at = @At(value = "TAIL"))
     private void animatium$onHitParticles(final CallbackInfo ci) {
-        if (AnimatiumSettings.visualSwing && AnimatiumSettings.INSTANCE.enabled && this.leftClickCounter > 0) {
+        if (AnimatiumSettings.INSTANCE.enabled && AnimatiumSettings.visualSwing && this.leftClickCounter > 0) {
             if (this.objectMouseOver != null && this.objectMouseOver.typeOfHit == MovingObjectPosition.MovingObjectType.ENTITY && !this.objectMouseOver.entityHit.hitByEntity(thePlayer) && this.objectMouseOver.entityHit instanceof EntityLivingBase) {
                 if (this.thePlayer.fallDistance > 0.0F && !this.thePlayer.onGround && !this.thePlayer.isOnLadder() && !this.thePlayer.isInWater() && !this.thePlayer.isPotionActive(Potion.blindness) && this.thePlayer.ridingEntity == null) {
                     this.thePlayer.onCriticalHit(this.objectMouseOver.entityHit);
@@ -83,24 +78,6 @@ public abstract class MinecraftMixin {
             }
 
             SwingHook.swingItem();
-        }
-    }
-
-    @Redirect(method = "clickMouse", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/PlayerControllerMP;clickBlock(Lnet/minecraft/util/BlockPos;Lnet/minecraft/util/EnumFacing;)Z"))
-    private boolean animatium$preventMiningWhenUsing(final PlayerControllerMP instance, final BlockPos blockPos, final EnumFacing facing) {
-        if (AnimatiumSettings.INSTANCE.enabled && AnimatiumSettings.oldBlockhitting && animatium$hasUseAction()) {
-            return false;
-        } else {
-            return instance.clickBlock(blockPos, facing);
-        }
-    }
-
-    @Redirect(method = "rightClickMouse", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/PlayerControllerMP;getIsHittingBlock()Z"))
-    private boolean animatium$enabledRightClick(final PlayerControllerMP instance) {
-        if (AnimatiumSettings.INSTANCE.enabled && AnimatiumSettings.oldBlockhitting && animatium$hasUseAction()) {
-            return false;
-        } else {
-            return instance.getIsHittingBlock();
         }
     }
 
@@ -125,11 +102,5 @@ public abstract class MinecraftMixin {
         if (AnimatiumSettings.funnyFidget && AnimatiumSettings.INSTANCE.enabled && this.thePlayer != null && this.thePlayer.getHeldItem() != null && this.thePlayer.getHeldItem().getItemUseAction() != EnumAction.NONE) {
             entityRenderer.itemRenderer.resetEquippedProgress();
         }
-    }
-
-    @Unique
-    private boolean animatium$hasUseAction() {
-        /* unironically, sk1er's old animations mod was on to something wtf */
-        return this.thePlayer.getHeldItem() != null && (this.thePlayer.getHeldItem().getItemUseAction() != EnumAction.NONE || this.thePlayer.getHeldItem().getItem() instanceof ItemBlock);
     }
 }
